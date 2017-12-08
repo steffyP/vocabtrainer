ca485df52f3eb5e84fea4db3b7d373666c1e2499
Steffy <steffy.p020@gmail.com>
2017-12-08 16:42:53 +0100

    initial commit

---
+++ app/src/main/java/com/vocabtrainer/project/vocabtrainer/api/OxfordApiRequest.java
@@ -0,0 +1,16 @@
+package com.vocabtrainer.project.vocabtrainer.api;
+
+
+import com.vocabtrainer.project.vocabtrainer.BuildConfig;
+
+/**
+ * Created by steffy on 26/11/2017.
+ */
+
+public class OxfordApiRequest {
+    private static final String ENDPOINT = "https://od-api.oxforddictionaries.com/api/v1";
+    private static final String API_KEY = BuildConfig.OXFORD_API_KEY;
+
+
+///entries/{source_translation_language}/{word_id}/translations={target_translation_language}
+}
